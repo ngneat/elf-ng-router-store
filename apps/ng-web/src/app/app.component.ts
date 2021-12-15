432165d3a74427729e816f8d09556a49c1a6ba0c
Boetzkes, Yannick <code@yannickboetzkes.com>
2021-12-15 15:24:41 +0100

    initial commit

---
+++ apps/ng-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngneat-elf-ng-router-store-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-web';
+}
